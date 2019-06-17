83083532541adf635f0b47f507b2940d81f9828f
Николай Перов <nikolayp2013@gmail.com>
2019-06-17 19:30:16 +0400

    initial project version

---
+++ reg/reg.php
@@ -0,0 +1,26 @@
+<?php
+  $username = trim(filter_var($_POST['username'], FILTER_SANITIZE_STRING));
+  $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
+  $login = trim(filter_var($_POST['login'], FILTER_SANITIZE_STRING));
+  $pass = trim(filter_var($_POST['pass'], FILTER_SANITIZE_STRING));
+
+  if(strlen($username) <= 3)
+    exit();
+  else if(strlen($email) <= 3)
+    exit();
+  else if(strlen($login) <= 3)
+    exit();
+  else if(strlen($pass) <= 3)
+    exit();
+
+  $hash = "ertewrtweewrtwertwetdfsvsdf";
+  $pass = md5($pass . $hash);
+
+  $user = 'root';
+  $password = 'root';
+  $db = 'testing';
+  $host = 'localhost';
+
+  $dsn = 'mysql:hosp='.$host.';dbname='.$db;
+  $pdo = new PDO($dsn, $user, $password);
+?>
