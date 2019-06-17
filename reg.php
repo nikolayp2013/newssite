b9f092a306a2d2cfda02a4e2e680aa8466ea44c6
Николай Перов <nikolayp2013@gmail.com>
2019-06-17 23:38:17 +0400

    initial project version

--- reg.php
+++ reg.php
@@ -13,7 +13,7 @@
     <div class="row">
       <div class="col-md-8 mb-3">
         <h4>Форма регистрации</h4>
-        <form action="reg/reg.php" method="post">
+        <form action="" method="post">
           <label for="username">Ваше имя</label>
           <input type="text" name="username" id="username" class="form-control">
 
@@ -26,7 +26,9 @@
           <label for="pass">Пароль</label>
           <input type="password" name="pass" id="pass" class="form-control">
 
-          <button type="submit" class="btn btn-success mt-5">
+          <div class="alert alert-danger mt-2" id="errorBlock"></div>
+
+          <button type="button" id="reg_user" class="btn btn-success mt-5">
             Зарегистрироваться
           </button>
         </form>
@@ -37,5 +39,32 @@
   </main>
 
   <?php require 'blocks/footer.php'; ?>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+  <script>
+    $('#reg_user').click(function () {
+      var name = $('#username').val();
+      var email = $('#email').val();
+      var login = $('#login').val();
+      var pass = $('#pass').val();
+
+      $.ajax({
+        url: 'reg/reg.php',
+        type: 'POST',
+        cache: false,
+        data: {'username' : name, 'email' : email, 'login' : login, 'pass' : pass},
+        dataType: 'html',
+        success: function(data) {
+          if(data == 'Готово') {
+            $('#reg_user').text('Все готово');
+            $('#errorBlock').hide();
+          } else {
+            $('#errorBlock').show();
+            $('#errorBlock').text(data);
+          }
+        }
+      });
+    });
+  </script>
 </body>
 </html>
---
+++ users.sql
@@ -0,0 +1,71 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 17, 2019 at 11:36 PM
+-- Server version: 5.7.24-log
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `login` varchar(70) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `pass` varchar(32) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `login`, `email`, `pass`) VALUES
+(1, 'Георгий', 'codi', 'gdudar999@gmail.com', 'ddb99e7e0905901c40f62fd8485d791d'),
+(2, 'Петр', 'Modest', 'test@test.ru', 'ddb99e7e0905901c40f62fd8485d791d'),
+(3, 'Андрей', 'codi999', 'test@test.ru', 'ae298cf9111465cc78e5b5143269ac10');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
