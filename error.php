907b022e62026b28e6df78c603ef52db2a7759e6
Nikolay Perov <nikolayp2013@gmail.com>
2019-06-17 09:36:10 +0400

    initial project version

---
+++ error.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Ошибка 404</title>
+</head>
+<body>
+  Ошибка 404, перейдите <a href="/">на главную</a>
+</body>
+</html>
